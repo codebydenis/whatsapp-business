df5574e368ea550a0d0d6f656e9828b8e37c85dd
Denis Patlin <dev.denispatlin@gmail.com>
2024-02-19 00:57:41 +0600

    lib(pinia): подключил pinia

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import './style.css';
 import App from './App.vue';
 
@@ -7,9 +8,10 @@ import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
+const pinia = createPinia();
 const vuetify = createVuetify({
 	components,
 	directives,
 });
 
-createApp(App).use(vuetify).mount('#app');
+createApp(App).use(pinia).use(vuetify).mount('#app');
