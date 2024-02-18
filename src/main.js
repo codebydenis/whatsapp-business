e6e7b4c8c9ac19f8d5b8f69d6eebcbb606e0c51e
Denis Patlin <dev.denispatlin@gmail.com>
2024-02-19 01:56:49 +0600

    font(mdi): подключил mdi иконки для vuetify

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router';
 import App from './App.vue';
 
 import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
